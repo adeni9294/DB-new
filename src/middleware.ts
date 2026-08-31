c1f5b33021357b71d731c46dd8e117b8548c34a4
adeni9294 <adeni9294@gmail.com>
2026-08-31 17:09:51 +0000

    Update middleware.ts
    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('sat_session_token')?.value;
   const { pathname } = request.nextUrl;
 
-  // 1. Proteksi Halaman Dashboard
-  if (pathname.startsWith('/dashboard') || pathname.startsWith('/structure') || pathname.startsWith('/events') || pathname.startsWith('/tools')) {
-    if (!token) {
-      const loginUrl = new URL('/login', request.url);
-      loginUrl.searchParams.set('callbackUrl', encodeURIComponent(pathname));
-      return NextResponse.redirect(loginUrl);
-    }
-  }
-
-  // 2. Redireksi Pengguna Berotentikasi dari Halaman Auth
-  if ((pathname === '/login' || pathname === '/register') && token) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
-
-  // 3. Tambahkan Security Headers pada Response
+  // MATIIN DULU CEK TOKEN. BIARIN SEMUA LEWAT
+  // NANTI CEK NYA KITA HANDLE DI DASHBOARD PAGE PAKE useEffect
+  
+  // 1. Cuma sisa Security Headers aja
   const response = NextResponse.next();
   response.headers.set('X-Frame-Options', 'DENY');
   response.headers.set('X-Content-Type-Options', 'nosniff');
